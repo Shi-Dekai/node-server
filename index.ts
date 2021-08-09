75ecf7f411a7605576154ea75e1ddda153302b3d
shi <1727259503@qq.com>
2021-08-09 23:14:38 +0800

    完成功能:根据不同æurl返回不同的文件

--- index.ts
+++ index.ts
@@ -1,28 +1,39 @@
 import * as http from 'http';
 import {IncomingMessage, ServerResponse} from 'http';
-import {Buffer} from 'buffer';
+import * as fs from 'fs';
+import * as p from 'path';
 
 const server = http.createServer();
+const publicDir = p.resolve(__dirname, 'public');
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   console.log('request.method', request.method);
   console.log('request.url:', request.url);
   console.log('request.headers', request.headers);
-
-  const array = [];
-  request.on('data', (chunk) => {
-    array.push(chunk);
-  });
-
-  request.on('end', () => {
-    const body = Buffer.concat(array).toString();
-    console.log('body:', body);
-
-    response.statusCode = 404
-    response.end();
-  });
+  const {method, url, headers} = request;
+  switch (url) {
+    case '/index.html':
+      response.setHeader('Content-Type', 'text/html; charset=utf-8')
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/style.css':
+      response.setHeader('Content-Type', 'text/css; charset=utf-8');
+      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/main.js':
+      response.setHeader('Content-type', 'text/javascript; charset=utf-8');
+      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+  }
 });
 
-server.listen(8888, () => {
-  console.log(server.address());
-});
+server.listen(8888);
